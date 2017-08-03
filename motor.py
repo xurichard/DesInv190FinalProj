83e109604d1f68f33727b34d72e7098129c37fd6
xurichard <richardxu@berkeley.edu>
2017-08-03 16:02:24 -0700

    added modules

--- motor.py
+++ motor.py
@@ -3,10 +3,16 @@ import RPi.GPIO as GPIO
 import time
 import datetime
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.OUT)
-pwm = GPIO.PWM(18, 100) #gnd and pin 18
-pwm.start(5)
+# Need the latest version of GPIO
+# $ sudo apt-get update
+# $ sudo apt-get dist-upgrade
+
+test = True
+if test:
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(18, GPIO.OUT)
+	pwm = GPIO.PWM(18, 100) #gnd and pin 18
+	pwm.start(5)
 
 class VisualApp:
 
@@ -21,27 +27,101 @@ class VisualApp:
         duty = float(angle) / 10.0 + 2.5
         pwm.ChangeDutyCycle(duty)
 
-class Dispenser:
+class Timer:
 
-	def __init__(self, master):
+	def __init__(self):
 		self.times = [] #filled with datetimes
 
-	def fill(time):
+	def add(time):
 		if type(time) == datetime:
 			self.times.append(time)
 
-	def dispense():
+	def check():
 		current = datetime.datetime.now()
 		for time in self.times:
-			if (current-time).total_seconds()/60 <= 900 #check if it's within 15 minutes of time
-				#allow dispensing
+			if (current-time).total_seconds()/60 <= 900: #check if it's within 15 minutes of time
 				return True
 		return False
 
+class Dispenser:
+
+	def __init__(self, pin=18, angle):
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setup(pin, GPIO.OUT)
+		self.pwm = GPIO.PWM(18, 100)
+		self.angle = angle
+		self.pin = pin
+
+	def dispense(slot, time):
+		self.pwm.start(slot*angle) #slot/total_slots
+		time.sleep(5) #wait for 5 seconds while dispensing
+		self.pwm.stop()
 
 
-root = Tk()
-root.wm_title('Servo Control')
-app = App(root)
-root.geometry("200x50+0+0")
-root.mainloop()
+class alarm():
+
+	def __init__(self, pin):
+		self.pin = pin
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setup(self.pin, GPIO.OUT)
+
+	def buzz(self, pitch, duration):
+		if(pitch==0):
+			time.sleep(duration)
+			return
+		period = 1.0 / pitch
+		delay = period / 2
+		cycles = int(duration * pitch)
+
+		for i in range(cycles):
+			GPIO.output(self.buzzer_pin, True)
+			time.sleep(delay)
+			GPIO.output(self.buzzer_pin, False)
+			time.sleep(delay)
+
+	def play(self):
+		pitches=[262,294,330,349,392,440,494,523,587,659,698,784,880,988,1047]
+		duration=0.1
+		for p in pitches:
+			self.buzz(p, duration)
+			time.sleep(duration *0.5)
+		for p in reversed(pitches):
+			self.buzz(p, duration)
+			time.sleep(duration *0.5)
+
+
+def main():
+
+
+if __name__ == "__main__":
+    if test:
+		root = Tk()
+		root.wm_title('Servo Control')
+		app = VisualApp(root)
+		root.geometry("200x50+0+0")
+		root.mainloop()
+	else:
+		main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
